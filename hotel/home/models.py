40f1dde630c1df41759778b58b879c0bc24645d8
sunny-sharma11 <sunnydecent11@gmail.com>
2020-04-05 08:35:32 +0530

    updation

--- hotel/home/models.py
+++ hotel/home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hotels(models.Model):
+
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to="static/upload")
+    location = models.CharField(max_length=30)
+    rating = models.IntegerField()
+    price = models.CharField(max_length=30)
+    tags = models.CharField(max_length=30)
+    is_feature = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+
+    mainhead = models.CharField(max_length=30)
+    head = models.CharField(max_length=30)
+    date = models.DateField()
+    desc = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="static/upload")
+    def __str__(self):
+        return self.mainhead
+
+
--- hotel/home/views.py
+++ hotel/home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Hotels
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-def hello(request):
-    return HttpResponse("hello world")
+
+    hotl = Hotels.objects.all()
+    return render(request, 'index.html', {'hotl':hotl})
